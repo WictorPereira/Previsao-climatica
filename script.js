29c945d39c79cc92797f285553d2d4a5526b2002
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2023-09-15 19:06:12 -0300

    Todos js Finalizados
     adicionado  async, await,  fetch  nas API utilizadas. tratamento de erro caso não exista a cidade e também tentar pesquisar sem digitar nada

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 //Variaveis 
 const apiKey = "d3a786cc430697df1e513525634e8900"
-const apiCountryFlag = `https://www.countryflagicons.com/SHINY/64/` 
+const apiCountryURL = `https://www.countryflagicons.com/SHINY/64/` 
 
 const cityInput = document.querySelector("#city-input")
 const searchBtn = document.querySelector("#search")
 
-//Funçoes
+const cityElement = document.querySelector("#city")
+const tempElement = document.querySelector("#temperature span")
+const descElement = document.querySelector("#description")
+const weatherIconElement = document.querySelector("#weather-icon")
+const countryElement = document.querySelector("#country")
+const humidityElement = document.querySelector("#humidity span")
+const windElement = document.querySelector("#wind span")
 
-const showWeatherData = (city) => {
-    console.log(city)
+const weatherContainer = document.querySelector("#weather-data")
+
+//Funçoes
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br` 
+
+    const res = await fetch (apiWeatherURL)
+    if(!res.ok){
+        alert('Cidade nao encontrada!')
+    }
+    const data = await res.json()
+    return data
+    
+}
+
+
+const showWeatherData = async (city) => {
+   const data = await getWeatherData(city)
+
+   cityElement.innerHTML = data.name
+   tempElement.innerHTML = parseInt(data.main.temp) 
+   descElement.innerHTML = data.weather[0].description
+   weatherIconElement.setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+   countryElement.setAttribute("src",apiCountryURL + data.sys.country + ".png")
+   humidityElement.innerHTML = `${data.main.humidity}%`
+   windElement.innerHTML = `${data.wind.speed} Km/h`
+   weatherContainer.classList.remove ('hide')
+
+
 }
 
 //Eventos
@@ -16,5 +49,26 @@ searchBtn.addEventListener("click", (e) => {
     e.preventDefault()
 
    const city = cityInput.value;
-   showWeatherData(city)
+   
+
+   if (city.trim() === "") {
+    alert("Digite uma cidade.");
+} else {
+    showWeatherData(city);
+}
+
 })
+
+cityInput.addEventListener("keyup", (e) => {
+    if(e.code === "Enter"){
+        const city = e.target.value
+        
+
+        if (city.trim() === "") {
+            alert("Digite uma cidade.");
+        } else {
+            showWeatherData(city);
+        }
+    }
+})
+
